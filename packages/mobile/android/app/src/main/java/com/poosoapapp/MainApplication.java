096243efa545cf22891b5ef019c3309273f0706a
unknown <yjkim5908@naver.com>
2023-03-20 21:13:55 +0900

    Add kakao login with @react-native-seoul/kakao-login

--- packages/mobile/android/app/src/main/java/com/poosoapapp/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/poosoapapp/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -62,4 +63,5 @@ public class MainApplication extends Application implements ReactApplication {
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
+
 }
